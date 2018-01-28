bc13c9ecb9a42bdcd21f6f24e9407af8966fc98e
LiunxPaisley <liunxalkaid@outlook.com>
2018-01-28 18:00:03 +0800

    2018年1月28日17点59分

--- Chapter 16. Template and inheritance/16.3 The Curiously Recurring Template Pattern/include/objectCounter.h
+++ Chapter 16. Template and inheritance/16.3 The Curiously Recurring Template Pattern/include/objectCounter.h
@@ -14,6 +14,26 @@ protected:
     {
         ++ObjectCounter<CountedType>::count;
     }
+    
+    // 拷贝构造函数
+    ObjectCounter(const ObjectCounter<CountedType>&)
+    {
+        ++ObjectedCounter<CountedType>::count;
+    }
+
+    // 析构函数
+    ~ObjectCounter()
+    {
+        --ObjectCounter<CountedType>::count;
+    }
+    public:
+    static size_t live()
+    {
+        return ObjectCounter<CountedType>::count;
+    }
 };
+
+template<typename CountedType>
+size_t ObjectCounter<CountedType>::count = 0;
 
 #endif // OBJECT_COUNTER_H_
