8e195c92ac75a2ccfdd02d270b9dd070b935a259
LiunxPaisley <liunxalkaid@outlook.com>
2018-02-01 17:35:59 +0800

    2018年2月1日17点35分

--- Chapter 20. Smart Pointer/20.2 Reference Counting/include/countingPtr.h
+++ Chapter 20. Smart Pointer/20.2 Reference Counting/include/countingPtr.h
@@ -2,31 +2,31 @@
 #define COUNTING_POINTER_H_
 
 template <typename T,
-    typename CounterPolicy = SimpleReferenceCount,
-    typename ObjectPolicy = StandardObjectPolicy>
+          typename CounterPolicy = SimpleReferenceCount,
+          typename ObjectPolicy = StandardObjectPolicy>
 class CountingPtr : private CounterPolicy, private ObjectPolicy
 {
-    private:
+private:
     typedef CounterPolicy CP;
     typedef ObjectPolicy OP;
 
-    T* object_pointed_to; // 所引用的对象
-    public:
+    T *object_pointed_to; // 所引用的对象
+public:
     // 缺省构造函数
     CountingPtr()
     {
         this->object_pointed_to = nullptr;
     }
-    
+
     // 一个针对转型的构造函数（转型自一个内建的指针）
-    explicit CountingPtr(T* p)
+    explicit CountingPtr(T *p)
     {
         this->init(p);
     }
 
     // 拷贝构造函数
-    CountingPtr(const Counting<T,CP,OP>& cp)
-    : CP((const CP&)cp), OP((const OP&)cp) // 拷贝policy
+    CountingPtr(const Counting<T, CP, OP> &cp)
+        : CP((const CP &)cp), OP((const OP &)cp) // 拷贝policy
     {
         this->attach(cp); // 拷贝指针，并增加计数值
     }
@@ -35,52 +35,52 @@ class CountingPtr : private CounterPolicy, private ObjectPolicy
     ~CountingPtr()
     {
         // 减少计数值，如果计数器为0，则释放该计数器
-        this->detach(); 
+        this->detach();
     }
 
     // 针对内建指针的赋值运算符
-    CountingPtr<T,CP,OP>& operator=(T* p)
+    CountingPtr<T, CP, OP> &operator=(T *p)
     {
         // 计数器不能指向*p
         assert(p != this->object_pointed_to);
         // 减少计数值，如果计数器为0，则释放该计数器
         this->detach();
         // 用一个普通指针进行初始化
-        this_>init(p);
+        this_ > init(p);
         return this;
     }
 
     // 拷贝赋值运算符，同时要考虑自己给自己赋值
-    CountingPtr<T,CP,OP>&
-    operator=(const CountingPtr<T,CP,OP>& cp)
+    CountingPtr<T, CP, OP> &
+    operator=(const CountingPtr<T, CP, OP> &cp)
     {
-        if(this->object_pointed_to!=cp.object_pointed_to)
+        if(this->object_pointed_to != cp.object_pointed_to)
         {
-            this->detach(); // 减少计数值，如果计数器为0，则释放该计数器
-            CP::operator=((const CP&)cp); // 对policy进行赋值
-            OP::operator=((const OP&)cp);
+            this->detach();                // 减少计数值，如果计数器为0，则释放该计数器
+            CP::operator=((const CP &)cp); // 对policy进行赋值
+            OP::operator=((const OP &)cp);
             this->attach(cp); // 拷贝指针并增加计数值
         }
         return this;
     }
 
     // 使之成为智能指针的运算符
-    T* operator=() const
+    T *operator=() const
     {
         return this->object_pointed_to;
     }
 
-    T& operator*() const 
+    T &operator*() const
     {
         return *this->object_pointed_to;
     }
 
-    private:
+private:
     // 辅助函数
     // 用普通指针进行初始化
-    void init(T* p)
+    void init(T *p)
     {
-        if(p!=nullptr)
+        if(p != nullptr)
         {
             CounterPolicy::init(p);
         }
@@ -88,7 +88,7 @@ class CountingPtr : private CounterPolicy, private ObjectPolicy
     }
 
     // 拷贝指针，并增加计数值
-    void attach(const CountingPtr<T,OP,CP>& cp)
+    void attach(const CountingPtr<T, OP, CP> &cp)
     {
         this->object_pointed_to = cp.object_pointed_to;
         if(cp.object_pointed_to != nullptr)
@@ -112,6 +112,43 @@ class CountingPtr : private CounterPolicy, private ObjectPolicy
             }
         }
     }
+
+    /**
+     * 定义一个到成员指针的转型
+     * 成员指针支持到bool类型的隐式转型，但是它和普通指针有区别，
+     * 因为对于delete操作或者指针算法而言，成员指针是无效的
+     */
+private:
+    class BoolConversionSupport
+    {
+        int dummy;
+    };
+
+public:
+    operator BoolConversionSupport::*() const
+    {
+        return this->object_pointed_to ? &BoolConversionSupport::dummy : 0;
+    }
+    /**
+     * 这两个运算符函数，必须依赖于模板参数，
+     * 只能实现为类内部的友元定义
+     */
+public:
+    friend bool operator==(const CountingPtr<T, CP, OP> &cp, const T *p)
+    {
+        return cp == p;
+    }
+    friend bool operator==(const T *p, const CountingPtr<T, CP, OP> &cp)
+    {
+        return p == cp;
+    }
 };
+
+template <typename T1, typename T2,
+          typename CP, typename OP>
+inline bool operator==(const CountingPtr<T1, CP, OP> &cp1, const CountingPtr<T2, CP, OP> &cp2)
+{
+    return cp1.operator->() == cp2.operator->();
+}
 
 #endif // COUNTING_POINTER_H_
--- Chapter 20. Smart Pointer/20.2 Reference Counting/include/simpleRefCount.h
+++ Chapter 20. Smart Pointer/20.2 Reference Counting/include/simpleRefCount.h
@@ -1,17 +1,67 @@
 #ifndef SIMPLE_REF_COUNT_H_
 #define SIMPLE_REF_COUNT_H_
 
-#include <cstddef>
+/**
+ * 非侵入式计数器
+ * 不把计数器存储在所指向对象的内部
+  */
+
 #include "allocator.h"
+#include <cstddef>
+
 
 class simpleRefCount
 {
-    private:
-    size_t* counter;
-    public:
+private:
+    /**
+     * counter的生命周期必须持续到最后一个智能指针被释放之后
+     * 所以使用一种特殊的分配器来完成这种任务：allocator.h
+     */
+    size_t *counter;
+
+public:
     simpleRefCount()
     {
-        
+        counter = nullptr;
+    }
+
+    // 缺省的拷贝构造函数和拷贝赋值运算符都是允许的
+    // 因为它们只是拷贝这个共享的计数器
+public:
+    // 分配计数器，并把它的值初始化为1
+    template <typename T>
+    void init(T *)
+    {
+        counter = alloc_counter();
+        *counter = 1;
+    }
+
+    // 释放该计数器
+    template <typename T>
+    void dispose(T *)
+    {
+        dealloc_counter(counter);
+    }
+
+    // 计数器值加1
+    template <typename T>
+    void increment(T *)
+    {
+        ++*counter;
+    }
+
+    // 计数值减1
+    template <typename T>
+    void decrement(T *)
+    {
+        --*counter;
+    }
+
+    // 检查计数值是否为0
+    template <typename T>
+    void is_zero(T *)
+    {
+        return *counter == 0;
     }
 };
 
