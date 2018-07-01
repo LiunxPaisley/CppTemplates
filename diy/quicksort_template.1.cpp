6ca90f1e3082996057b750540034707cea8b7ecd
LiunxPaisley <liunxalkaid@outlook.com>
2018-07-01 18:08:25 +0800

    2018-07-01-Sunday-18:12

--- diy/quicksort_template.1.cpp
+++ diy/quicksort_template.1.cpp
@@ -39,12 +39,12 @@ struct swap
 //    }
 };
 template <int i, int j>
-struct swap<true>
+struct swap<true, i, j>
 {
     //enum {value = i+1};
-    int operator()(float* data)
+    static int ret(float* data)
     {
-        T temp = data[j];
+        float temp = data[j];
         data[j] = data[i];
         data[i] = temp;
         return i+1;
@@ -52,10 +52,10 @@ struct swap<true>
 };
 
 template <int i, int j>
-struct swap<false>
+struct swap<false, i, j>
 {
 //    enum {value = i};
-    int operator()(float* data)
+    static int ret(float* data)
     {
         return i;
     }
@@ -67,21 +67,22 @@ struct partitionLoop
 };
 
 template <int i, int j, int x>
-struct partitionLoop<true>
+struct partitionLoop<true, i, j, x>
 {
-    int operator()(float* data)
+//    enum {value = };
+    static int ret(float* data)
     {
-        int i_swap = swap<data[j]<data[x], i, j>();
+        int i_swap = swap<data[j]<data[x], i, j>::ret(data);
 //        int i_swap = swap<data[j]<data[x], i, j>::value;
-        return partitionLoop<j+1<x, i_swap, j+1, x>();
+        return partitionLoop<j+1<x, i_swap, j+1, x>::ret(data);
     }
 };
 
 template <int i, int j, int x>
-struct partitionLoop<false>
+struct partitionLoop<false, i, j, x>
 {
-    int operator()(float* data) {
-        return swap<data[j]<data[x], i, j>();
+    static int ret(float* data) {
+        return swap<data[j]<data[x], i, j>::ret(data);
     }
 };
 
@@ -89,9 +90,9 @@ template <int a, int b>
 struct partition
 {
     //enum {i = a - 1, j = 0};
-    int operator()(float* data)
+    static int ret(float* data)
     {
-        return partitionLoop<true, a, a, b>(data);
+        return partitionLoop<true, a, a, b>::ret(data);
     }
 };
 
@@ -99,11 +100,11 @@ template <int a, int b>
 struct quickSort
 {
     //enum { i = a - 1, j = 0};
-    void operator()(float* data)
+    static void func(float* data)
     {
-        int p = partition<a, b>(data);
-        quickSort<a, p>(data);
-        quickSort<p, b>(data);
+        const int p = partition<a, b>::ret(data);
+        quickSort<a, p>::func(data);
+        quickSort<p, b>::func(data);
     }
 };
 
@@ -112,23 +113,23 @@ int main()
     const int num = 10;
     const int a   = -50;
     const int b   = 50;
-    float     data[num];
+    float     data[num] = {7.4, 9.6, 8.3, 4.2, 5.1, 3.3, 1.9, 2.7, 3.7, 5.0};
 
     printf("Before sort, data = ");
-    srand(static_cast<unsigned int>(time(NULL)));
+//    srand(static_cast<unsigned int>(time(NULL)));
     for (size_t i = 0; i < num; i++)
     {
-        data[i] = rand() * ((b - a) / RAND_MAX) + a;
-        printf("lf% ", data[i]);
+//        data[i] = rand() * ((b - a) / RAND_MAX) + a;
+        printf("%lf ", data[i]);
     }
     printf("\n");
 
-    quicksort<num>(data);
+    quickSort<0, num - 1>::func(data);
 
     printf("After sort, data = ");
     for (size_t j = 0; j < num; j++)
     {
-        printf("lf% ", data[j]);
+        printf("%lf ", data[j]);
     }
     printf("\n");
 }
